c77faf704c755041d546608b45ad2f708dcc0528
kanika-07cs <kanikaab.cs24@bitsathy.ac.in>
2025-02-17 10:21:31 +0530

    changed

--- src/pages/Icecream.jsx
+++ src/pages/Icecream.jsx
@@ -1,24 +1,36 @@
-import React from 'react'
-import "../styles/icecream.css"
-import blackCurrent from "../assets/Black-Currant-Flavour.jpg"
-import chocolate from "../assets/chocolate-chip-ice-cream.jpg"
-import mint from "../assets/Mint-Chocolate-Chip-Ice-Cream.jpg"
-import redVelvet from "../assets/red-velvet-ice-cream.webp"
-import oreo from "../assets/ice-cream-oreo-cookie.jpg"
-import butterscotch from "../assets/Butterscotch-cookie-dough-ice-cream.jpg"
+import React, { useState, useEffect } from "react";
+import "../styles/icecream.css";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
+import blackCurrent from "../assets/Black-Currant-Flavour.jpg";
+import chocolate from "../assets/chocolate-chip-ice-cream.jpg";
+import mint from "../assets/Mint-Chocolate-Chip-Ice-Cream.jpg";
+import redVelvet from "../assets/red-velvet-ice-cream.webp";
+import oreo from "../assets/ice-cream-oreo-cookie.jpg";
+import butterscotch from "../assets/Butterscotch-cookie-dough-ice-cream.jpg";
+
+const images = [blackCurrent, chocolate, mint, redVelvet, oreo, butterscotch];
 
 export default function Icecream() {
-  return (
-    <div className='icecream'>
-        <div className='flavours'>
-      <img src={blackCurrent} alt="blackCurrent" width="300px" height="300px"></img>
-      <img src={chocolate} alt="chocolate" width="300px" height="300px"></img>
-      <img src={mint} alt="mint" width="300px" height="300px"></img>
-      <img src={redVelvet} alt="redVelvet" width="300px" height="300px"></img>
-      <img src={oreo} alt="oreo" width="300px" height="300px"></img>
-      <img src={butterscotch}alt="butterscotch" width="300px" height="300px"></img>
-      </div>
+  const [currentIndex, setCurrentIndex] = useState(0);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000); // Change image every 3 seconds
+
+    return () => clearInterval(interval); // Cleanup interval on unmount
+  }, []);
+
+  return (
+    <div className="icecream">
+        <LazyLoadImage
+          src={images[currentIndex]}
+          alt="Ice Cream"
+          width="300px"
+          height="300px"
+          effect="blur" // Lazy loading with blur effect
+        />
     </div>
-  )
+  );
 }
